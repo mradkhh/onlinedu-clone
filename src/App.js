1890bc1cdb2a7bb72773d35d8a79a0e7984ad695
AdkhambekDeveloper <adkhambekyusupov@gmail.com>
2022-04-11 20:41:31 +0500

    create

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Home from "./Pages/Home/Home";
+import "antd/dist/antd.css";
 
 function App() {
   return (
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,17 +1,45 @@
 import React from 'react'
 import styles from './Header.module.scss'
-import { Space, Layout } from 'antd'
-import { Button } from '../Button'
+import { Space, Layout, Input, Dropdown, Button, Menu, message } from 'antd'
+import { DownOutlined, UserOutlined } from '@ant-design/icons';
+
 
 const Header = () => {
     const {Header} = Layout;
+    const { Search } = Input;
+
+    const handleMenuClick = (e) => {
+      message.info('Click on menu item.');
+      console.log('click', e);
+    }
+
+    const menu = (
+      <Menu onClick={handleMenuClick}>
+        <Menu.Item key="1" icon={<UserOutlined />}>
+          1st menu item
+        </Menu.Item>
+        <Menu.Item key="2" icon={<UserOutlined />}>
+          2nd menu item
+        </Menu.Item>
+        <Menu.Item key="3" icon={<UserOutlined />}>
+          3rd menu item
+        </Menu.Item>
+      </Menu>
+    );
+
+
   return (
     <Header>
-        <Space>
+        <Space direction="horizontal" align='center'>
             <div>
                 <img src="/Images/logo.png" alt="logo" />
             </div>
-            <Button type={'primary-outline'} text={'Категории'}/>
+            <Dropdown overlay={menu} trigger={['click']}>
+              <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                Click me <DownOutlined />
+              </a>
+            </Dropdown>
+            <Search placeholder='Что Вы хотите изучать?' />
         </Space>
     </Header>
   )
