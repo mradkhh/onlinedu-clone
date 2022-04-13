e95db7dd4f7e5a8359ac9ccc9ce832785f3d1937
AdkhambekDeveloper <adkhambekyusupov@gmail.com>
2022-04-14 00:24:57 +0500

    build

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import Home from "./Pages/Home/Home";
 import "antd/dist/antd.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <>
-        <Home />
-      </>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
