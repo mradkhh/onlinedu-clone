45be411570760ed59d3636548a3d82ff9c960e76
Adkhambek <adkhambekyusupov@gmail.com>
2022-04-16 00:40:08 +0500

    fix

--- src/Pages/Home/Sections/About.jsx
+++ src/Pages/Home/Sections/About.jsx
@@ -11,22 +11,22 @@ const About = () => {
             <p className="about__info-subtitle">Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев более менее осмысленного текста рыбы на русском языке.</p>
           </div>
           <div className="row about__items">
-            <div className="col-12 col-md-6 col-lg-3 about__items-child">
+            <div className="col-12 col-md-6 col-lg-3 col-xl-3 about__items-child">
               <img src="/Images/about1.png" alt="about" />
               <h5>Легко используемый интерфейс</h5>
               <p>Сайт рыбатекст поможет вам и дизайнеру, версталь, вебмастеру сгенерировать несколько абзацев</p>
             </div>
-            <div className="col-12 col-md-6 col-lg-3 about__items-child">
+            <div className="col-12 col-md-6 col-lg-3 col-xl-3  about__items-child">
               <img src="/Images/about2.png" alt="about" />
               <h5>Квалифицированные специалисты</h5>
               <p>Сайт рыбатекст поможет вам и дизайнеру, версталь, вебмастеру сгенерировать несколько абзацев</p>
             </div>
-            <div className="col-12 col-md-6 col-lg-3 about__items-child">
+            <div className="col-12 col-md-6 col-lg-3 col-xl-3  about__items-child">
               <img src="/Images/about3.png" alt="about" />
               <h5>Доступный и понятный видео-материал </h5>
               <p>Сайт рыбатекст поможет вам и дизайнеру, версталь, вебмастеру сгенерировать несколько абзацев</p>
             </div>
-            <div className="col-12 col-md-6 col-lg-3 about__items-child">
+            <div className="col-12 col-md-6 col-lg-3 col-xl-3  about__items-child">
               <img src="/Images/about4.png" alt="about" />
               <h5>20 000+ курсов по разным направлениям</h5>
               <p>Сайт рыбатекст поможет вам и дизайнеру, версталь, вебмастеру сгенерировать несколько абзацев</p>
