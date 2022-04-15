43fc317ae1c9bb2a47aaf0ccaac5d71c84724955
Adkhambek <adkhambekyusupov@gmail.com>
2022-04-15 17:01:53 +0500

    create and fix

--- src/Components/Cards/Card.jsx
+++ src/Components/Cards/Card.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import './Card.scss'
 
-const Card = () => {
+const Card = ({image, title, price, science, rating, choise}) => {
   return (
-    <div className="col col-md-2 col-lg-3">
+    <div className="col-12 col-md-6 col-lg-4">
       <div className="card">
       <div className="card-img">
-
+        <img src={image} alt="science" />
+        <div className="more">
+        <span>{science}</span>
+        <div className="rating">
+          <img src="/Images/full-star.png" alt="star" />
+          <img src="/Images/full-star.png" alt="star" />
+          <img src="/Images/full-star.png" alt="star" />
+          <img src="/Images/full-star.png" alt="star" />
+          <img src="/Images/empty-star.png" alt="star" />
+        </div>
+        </div>
+      </div>
+      <div className="card__body">
+        <div className="card__body-title">{title}</div>
+        <div className="card__body-more">
+          <span>{price}</span>
+          <button data-type={choise ? 'disabled' : 'primary'}>Выбрать</button>
+        </div>
       </div>
     </div>
     </div>
--- src/Pages/Home/Sections/Popular.jsx
+++ src/Pages/Home/Sections/Popular.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from '../../../Components/Cards/Card'
+import './Popular.scss'
 
 const Popular = () => {
   return (
@@ -7,8 +8,55 @@ const Popular = () => {
       <div className="container">
         <div className="popular">
           <h2 className="popular__title">Популярные курсы</h2>
-          <div className="row">
-            <Card/>
+          <div className="row flex-wrap justify-content-between">
+            <Card
+            image={"/Images/card1.png"}
+            title={"Маркетинг и все его тонкости. Профессия маркетолога с 0 до эксперта "}
+            price={"95 000 сум"}
+            science={"Маркетинг"}
+            rating={4}
+            choise={false}
+            />
+            <Card
+            image={"/Images/card1.png"}
+            title={"Маркетинг и все его тонкости. Профессия маркетолога с 0 до эксперта "}
+            price={"95 000 сум"}
+            science={"Маркетинг"}
+            rating={4}
+            choise={false}
+            />
+            <Card
+            image={"/Images/card1.png"}
+            title={"Маркетинг и все его тонкости. Профессия маркетолога с 0 до эксперта "}
+            price={"95 000 сум"}
+            science={"Маркетинг"}
+            rating={4}
+            choise={false}
+            />
+            <Card
+            image={"/Images/card1.png"}
+            title={"Маркетинг и все его тонкости. Профессия маркетолога с 0 до эксперта "}
+            price={"95 000 сум"}
+            science={"Маркетинг"}
+            rating={4}
+            choise={false}
+            />
+            <Card
+            image={"/Images/card1.png"}
+            title={"Маркетинг и все его тонкости. Профессия маркетолога с 0 до эксперта "}
+            price={"95 000 сум"}
+            science={"Маркетинг"}
+            rating={4}
+            choise={false}
+            />
+            <Card
+            image={"/Images/card1.png"}
+            title={"Маркетинг и все его тонкости. Профессия маркетолога с 0 до эксперта "}
+            price={"95 000 сум"}
+            science={"Маркетинг"}
+            rating={4}
+            choise={false}
+            />
           </div>
         </div>
       </div>
