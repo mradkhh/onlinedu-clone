190f576d37ce3377c089895cafd895429e3023a6
AdkhambekDeveloper <adkhambekyusupov@gmail.com>
2022-04-27 10:40:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,22 +16,47 @@ import {
   Vebinar,
   Vebinar2,
 } from 'Pages/index';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import option from 'Services/Aos';
+import Request from 'Services/Request';
 import './App.css';
 import './i18n';
 
 function App() {
+  const [userInfo, setUserInfo] = useState([])
+  const [userPosition, setUserPosition] = useState([])
+  const [categories, setCategories] = useState([])
+
+
   useEffect(() => {
     AOS.init({ ...option });
+    //profil.get request
+    Request()
+      .get('/profil/get')
+      .then((res) => {
+        setUserInfo(res?.data)
+      })
+      .catch(err => console.log(err))
+      Request()
+      .get('/profil/user-position')
+      .then((res) => {
+        setUserPosition(res?.data)
+      })
+      .catch(err => console.log(err))
+      Request()
+      .get('/dashboard/categories')
+      .then((res) => {
+        setCategories(res?.data)
+      })
+      .catch(err => console.log(err))
   }, []);
   return (
     <I18nextProvider>
       <BrowserRouter>
-        <ToastContainer />
+        <ToastContainer/>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/categories' element={<Categories />} />
