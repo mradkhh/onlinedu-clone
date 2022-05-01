b384e70544608cda60214cff58e56951fc6761c2
AdkhambekDeveloper <adkhambekyusupov@gmail.com>
2022-05-01 11:34:49 +0500

    fix font family bug

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import './index.css';
 import './global.scss';
+import './Fonts/stylesheet.css'
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
-  
