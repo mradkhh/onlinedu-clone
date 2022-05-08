675ddeda4523c4d2bd0b1d8bef190808c96f1dec
AdkhambekDeveloper <adkhambekyusupov@gmail.com>
2022-05-08 17:19:28 +0500

    fix redux store

--- src/store/reducers/courseLessonReducer.js
+++ src/store/reducers/courseLessonReducer.js
@@ -1,47 +1,50 @@
 import { ADD_TO_LESSON, REMOVE_FROM_LESSON } from '../constants/actionTypes'
 import { omit } from 'lodash'
-import { getLocalStorage } from '../../utils/localStorage'
-
-const initialState = [
-  {
-    courseTitle: 'Birinchi kurs',
-    courseId: 1,
-    lessons: [
-      {
-        item: 'bir'
-      },
-      {
-        item: 'ikki'
-      },
-      {
-        item: 'uch'
-      },
-    ]
-  },
-  {
-    courseTitle: 'Ikkinchi kurs',
-    courseId: 2,
-    lessons: [
-      {
-        item: 'bir'
-      },
-      {
-        item: 'ikki'
-      },
-      {
-        item: 'uch'
-      },
-    ]
-  }
-]
-
+import { getLocalStorage } from 'utils/localStorage'
+const getStateFromLocalStorage = getLocalStorage('store')
+const customState = {
+  title: 'onlinedu-course',
+  data: [
+    {
+      courseTitle: 'Birinchi kurs',
+      courseId: 1,
+      lessons: [
+        {
+          item: 'bir'
+        },
+        {
+          item: 'ikki'
+        },
+        {
+          item: 'uch'
+        },
+      ]
+    },
+    {
+      courseTitle: 'Ikkinchi kurs',
+      courseId: 2,
+      lessons: [
+        {
+          item: 'bir'
+        },
+        {
+          item: 'ikki'
+        },
+        {
+          item: 'uch'
+        },
+      ]
+    }
+  ]
+}
+const initialState = getStateFromLocalStorage ? getStateFromLocalStorage : customState
 const courseLessonReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_LESSON:
-      return {
-        ...state,
-        ...action.payload
-      }
+      return(
+        {...state,
+        ...action.payload}
+      )
     case REMOVE_FROM_LESSON:
       return omit(state, [action.payload])
 
